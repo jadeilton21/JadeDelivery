aa9914383de4cd644d5daf9b98a425908077e612
jadeilton <jadeiltondm@gmail.com>
2025-07-21 21:15:57 -0300

    Initial commit

---
+++ Microserviços/Courier-Management/src/test/java/com/jadeilton/microservicos/courier/management/CourierManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadeilton.microservicos.courier.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Microserviços/Delivery-Tracking/src/main/java/com/jadeilton/microservicos/delivery/tracking/DeliveryTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.jadeilton.microservicos.delivery.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryTrackingApplication.class, args);
+	}
+
+}
