f479995ae00f30339e1d657bfdc4316c31ceeb4b
jadeilton <jadeiltondm@gmail.com>
2025-07-23 14:24:46 -0300

    create class ContactPoint

---
+++ Microserviços/Delivery-Tracking/src/main/java/com/jadeilton/microservicos/delivery/tracking/domain/model/ContactPoint.java
@@ -0,0 +1,24 @@
+package com.jadeilton.microservicos.delivery.tracking.domain.model;
+
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Embeddable
+@EqualsAndHashCode
+@AllArgsConstructor
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+public class ContactPoint {
+
+
+
+
+    private String street;
+    private String number;
+    private String complement;
+    private String name;
+    private String phone;
+    private String zipCode;
+}
